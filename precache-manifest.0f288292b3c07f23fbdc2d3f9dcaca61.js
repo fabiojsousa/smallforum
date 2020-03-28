fbe065ccc6aa2131f943b517a435726622d20ce1
Fábio Sousa <fabiojdev@gmail.com>
2020-03-27 21:36:25 -0300

    Updates

---
+++ precache-manifest.0f288292b3c07f23fbdc2d3f9dcaca61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "405771ef3e748a2b057f4a137c0cbe89",
+    "url": "/smallforum/index.html"
+  },
+  {
+    "revision": "5c5e658b7d1cbe86fab9",
+    "url": "/smallforum/static/js/2.0869fde3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/smallforum/static/js/2.0869fde3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a41aaa58d10af353596",
+    "url": "/smallforum/static/js/main.e13baead.chunk.js"
+  },
+  {
+    "revision": "c3028d7e1ebbdfde93d8",
+    "url": "/smallforum/static/js/runtime-main.64657aa6.js"
+  },
+  {
+    "revision": "018d2ac351d2f2de9b77e870f29894e1",
+    "url": "/smallforum/static/media/loading.018d2ac3.gif"
+  }
+]);
